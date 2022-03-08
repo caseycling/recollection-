7c8b7981f95f07b1c2614b52a449083f999e930d
Casey <caseyclinga@gmail.com>
2022-03-08 12:19:36 -0800

    Removed console log

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,13 +3,12 @@ import PostMessage from '../models/postMessage.js';
 //Get posts
 export const getPosts = async (req, res) => {
   try {
-    const postMessages = PostMessage.find();
+    const postMessages = await PostMessage.find();
 
     console.log(postMessages);
 
     res.status(200).json(postMessages);
   } catch (error) {
-    console.log('ERROR!!!');
     console.log(error.message);
     res.status(404).json({ message: error.message });
   }
