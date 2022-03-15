59f2738b342b0405f92a796dbdf44532d60a1082
Casey <caseyclinga@gmail.com>
2022-03-15 12:57:17 -0700

    Created additional UI and implemented hooks

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 import useStyles from './styles';
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
   const classes = useStyles();
-  return <h1>Form</h1>;
+
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete='off'
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit()}
+      >
+        <Typography variant='h6'>Creating a memory</Typography>
+        <TextField
+          name='creator'
+          vairant='outlined'
+          label='Creator'
+          fullwidth
+          value={postData.creator}
+          onChange={(e) => {
+            setPostData({ ...postData, creator: e.target.value });
+          }}
+        />
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
