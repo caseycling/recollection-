49d07e6016bd6b8d66ba962e98a120ca9405944f
Casey <caseyclinga@gmail.com>
2022-03-23 12:56:47 -0700

    Refactored createPost controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -14,15 +14,35 @@ export const getPosts = async (req, res) => {
   }
 };
 
-//Create posts
-export const createPost = (post) => async (dispatch) => {
-  const body = req.body;
+// //Create posts
+// export const createPost = (post) => async (dispatch) => {
+//   const body = req.body;
 
-  const newPost = new PostMessage(post);
+//   const newPost = new PostMessage(post);
+//   try {
+//     await newPost.save();
+
+//     res.status(201).json(newPost);
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// };
+
+export const createPost = async (req, res) => {
+  const { title, message, selectedFile, creator, tags } = req.body;
+
+  const newPostMessage = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
+
   try {
-    await newPost.save();
+    await newPostMessage.save();
 
-    res.status(201).json(newPost);
+    res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
