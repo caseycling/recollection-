028121e562f018209118d804e75e41bca620d6c3
Casey <caseyclinga@gmail.com>
2022-04-03 14:47:14 -0700

    Backend logic for liking posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -54,3 +54,20 @@ export const deletePost = async (req, res) => {
 
   res.json({ message: 'Post deleted succesfully' });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send('No post with that id');
+  }
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount++ },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
