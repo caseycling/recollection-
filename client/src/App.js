6b871aa1ffa9ec34cbe3b8ea72784c074c2e3419
Casey <caseyclinga@gmail.com>
2022-05-24 13:15:55 -0700

    Changed title

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography className={classes.heading} variant='h2' align='center'>
-          Memories
+          Reminiscence
         </Typography>
         <img
           className={classes.image}
