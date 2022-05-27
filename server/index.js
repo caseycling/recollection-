4ad78f4a2eb61127f369a50be4b62cbfef807ffc
Casey <caseyclinga@gmail.com>
2022-05-27 16:31:12 -0700

    Added code to serve files from express server

--- server/index.js
+++ server/index.js
@@ -18,7 +18,16 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL =
   'mongodb+srv://caseyclinga:123abc@passport.czhpd.mongodb.net/recollections?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 mongoose
   .connect(CONNECTION_URL, {
