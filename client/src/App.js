317066114508ae11f36148edbacdf1883728554c
Casey <caseyclinga@gmail.com>
2022-05-28 13:13:35 -0700

    Title change

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography className={classes.heading} variant='h2' align='center'>
-          Reminiscence
+          Remi
         </Typography>
         <img
           className={classes.image}
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import path, { dirname } from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import postRoutes from './routes/posts.js';
@@ -17,20 +17,20 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-  'mongodb+srv://caseyclinga:123abc@passport.czhpd.mongodb.net/recollections?retryWrites=true&w=majority';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+const CONNECTION_URL =
+  'mongodb+srv://caseyclinga:123abc@passport.czhpd.mongodb.net/recollections?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
