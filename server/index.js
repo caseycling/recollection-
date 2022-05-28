b92ff0263433edc61cb824d47838c2aa2c949532
Casey <caseyclinga@gmail.com>
2022-05-27 17:15:52 -0700

    removed var

--- server/index.js
+++ server/index.js
@@ -24,7 +24,6 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express serve up index.html file if it doesn't recognize route
   const path = import('path');
-  const __dirname = path.resolve(path.dirname(''));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
